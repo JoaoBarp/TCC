441a0eb67532db48e0b2a22b501dbc8b05f47fe3
JoaoBarp <joaorbarp@hotmail.com>
2021-05-10 08:51:49 -0300

    .
    .

--- RandomForest.py
+++ RandomForest.py
@@ -18,7 +18,7 @@ import sys
 SEED =  19963
 # load data
 filename='C:\\Users\\joaor\\Desktop\\Databases\\' + sys.argv[1]
-filesalve='C:\\Users\\joaor\\Desktop\\' + sys.argv[2]
+filesalve='C:\\Users\\joaor\\Desktop\\TCC\\Result' + sys.argv[2]
 perg = ['CountPalavrasBody','CountPalavrasTitle','Nfrasesbody','flesch_reading_ease','mediaCaracteresFrase','tamCod','interogacao','iniciaWH','subjectivity','polaridade','sumT','NpergFei','NresFei','Rotulo']
 perg2 = ['N Palavras corpo','N Palavras Título','N frases corpo','flesch_reading_ease','Média Caracteres Frase','Tamanho Código','Interogacão','Inicia com WH','Subjetividade','Polaridade','N de tags','N perguntas Feitas','N respostas Feitas','Rótulo']
 
