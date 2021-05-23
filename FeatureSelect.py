59b23768d166034fc1240a41e2a509c203fee351
JoaoBarp <joaorbarp@hotmail.com>
2021-05-23 19:59:05 -0300

    .
    .

--- FeatureSelect.py
+++ FeatureSelect.py
@@ -10,21 +10,21 @@ from matplotlib import pyplot
 from sklearn.ensemble import RandomForestClassifier
 
 # load data
-filename = 'DataParaTreino.csv'
-perg = ['CountPalavrasBody','CountPalavrasTitle','Nfrasesbody','flesch_reading_ease','mediaCaracteresFrase','tamCod','interogacao','iniciaWH','subjectivity','polaridade','sumT','NpergFei','NresFei','Rotulo']
-perg2 = ['N Palavras corpo','N Palavras Título','N frases corpo','flesch_reading_ease','Média Caracteres Frase','Tamanho Código','Interogacão','Inicia com WH','Subjetividade','Polaridade','N de tags','N perguntas Feitas','N respostas Feitas','Rótulo']
+filename = 'C:\\Users\\joaor\\Desktop\\Databases\\XX.csv'
+#perg = ['CountPalavrasBody','CountPalavrasTitle','Nfrasesbody','flesch_reading_ease','mediaCaracteresFrase','tamCod','interogacao','iniciaWH','subjectivity','polaridade','sumT','NpergFei','NresFei','Rotulo']
+perg2 = ['N Palavras corpo','N Palavras Titulo','N frases corpo','flesch','Media Caracteres Frase','Tamanho Codigo','Interogacao','Inicia com WH','Subjetividade','Polaridade','N de tags','N perguntas Feitas','N respostas Feitas','Rotulo']
 
 
 print('Começou...')
-dataframe=pd.read_csv(filename,sep='\t',usecols=perg, encoding='utf-8')
-dataframe.columns=perg2
+dataframe=pd.read_csv(filename,sep='\t',usecols=perg2, encoding='utf-8')
+#dataframe.columns=perg2
 print('Acabou...')
 
 dataframe=dataframe.dropna()
 
 
-X = dataframe.drop(columns=['Rótulo'])
-y = dataframe['Rótulo']
+X = dataframe.drop(columns=['Rotulo'])
+y = dataframe['Rotulo']
 
 '''
 #-----------------------------------------------AQUI FOI f_classif ------------------------------------------------------------------------------------------------------
@@ -64,13 +64,13 @@ pyplot.show()
 
 var=[]
 
-#fs = SelectKBest(score_func=f_classif, k='all')
-fs = SelectFpr(score_func=f_classif)
+fs = SelectKBest(score_func=f_classif, k='all')
+#fs = SelectFpr(score_func=f_classif)
 # learn relationship from training data
 fs.fit(X,y)
 # transform train input data
 for i in range(len(fs.scores_)):
-    print(perg[i],':         ', fs.scores_[i])
+    print(perg2[i],':         ', fs.scores_[i])
     var.append(fs.scores_[i])
 
 # plot the scores
@@ -89,10 +89,10 @@ sampledata = {'genre': ['N Palavras corpo','N Palavras Título','N frases corpo'
 test = pd.DataFrame(sampledata,  index=sampledata['genre'])
 test.sort_values(by = "rating").plot(kind = "barh", color = "blue", legend = False, grid = True, ax = ax)
 
-pyplot.xlim(0, 30000)
+pyplot.xlim(0, 350000)
 pyplot.xlabel('Score')
 pyplot.ylabel('Features')
-pyplot.title('SelectFpr(f_classif)')
+pyplot.title('SelectKBest')
 for i, v in enumerate(sorted(test.rating)):
     pyplot.text(v+0.2, i, str(round(v, 2)), color='blue', va="center")
 
@@ -102,7 +102,7 @@ list=z.index.tolist()
 list=list[::-1]
 print(list)
 pyplot.show()
-arquivo = open('seque.txt', 'w+')
+arquivo = open('seque4class.txt', 'w+')
 for x in list:
     arquivo.writelines(x)
     arquivo.writelines(',')
